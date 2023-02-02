Changed type_damaage_avg() to splt df by type and average out damage by type, dual types might interfere though, no clue rn how to best fix

pokedex_graphs/pokedex_vis.py
@@ -15,4 +15,12 @@ if __name__ == '__main__':
         plt.show()
 
     def type_damage_avg():
-        pass
+       print(data['Type 1'].unique())
+       # for x in data['Type 1'].unique():
+       #     z = np.average(data.loc[data['Type 1' or 'Type 2'] == x]['Attack'])
+       #     print(z)
+
+    type_damage_avg()    
+    #input('''What do you want to display?
+#(1) Dual Type count
+#(2) Average of Damage by Type\n''')