2d090f37e395cc7ce1ebd7dfc0dd30832b16d64e
Reox-Fox <freox@yahoo.com>
2023-02-03 14:41:55 -0900

    Completely redid Congress_datavis.py, and added plot specifics to pokemon_viz.py

--- 117th_Congress/congress_datavis.py
+++ 117th_Congress/congress_datavis.py
@@ -7,45 +7,31 @@ from os import getcwd
 
 if __name__ == '__main__':
     data = pd.read_csv(Path(getcwd(),'data','congress.csv'))
-
-    age = data['Age'].dropna()
-    unique_age = np.sort(age.unique())
-    age_count = [age.value_counts()[x] for x in unique_age]
-    age_quartiles = []
-    age_quartiles = np.quantile(age,[.25,.5,.75])
-
-    parties = data['Party']
-    unique_parties = parties.unique()
-    party_count = [parties.value_counts()[x] for x in unique_parties]
-    party_count = {k:v for (k,v) in zip(unique_parties, party_count)}
-
-
-    terms = data['Number of Terms'].dropna()
-    unique_terms = np.sort(terms.unique())
-    terms_count = [terms.value_counts()[x] for x in unique_terms]
-
+    z = np.arange(5, 26, 5)
 
     def age_distribution():
-        plt.bar(unique_age,age_count, zorder=5, label='congress age distribution')
+        age = data['Age'].dropna()
+        republican = data.loc[data.Party == 'Republican'].Age
+        democrat = data.loc[data.Party == 'Democrat'].Age
+        other = data.loc[(data.Party != 'Republican') &
+            (data.Party != 'Democrat')]
+        quartiles = np.quantile(age, [.25, .5, .75])
 
-        plt.xlabel('Age of Congress')
-        plt.xticks(np.arange(25,91,5))
-
-        plt.ylabel('Number of people')
-        plt.yticks(np.arange(0,24,2))
-    
-        plt.axvline(age_quartiles[0], color='r', zorder=10, label="Quartiles")
-        for x in age_quartiles[1:]:
-            plt.axvline(x, color='r', zorder=10)
-
+        plt.bar(age.unique(), age.value_counts(), zorder=z[-2])
+        plt.xticks(np.arange(25, 89, 3))
+        plt.yticks(np.arange(2, 23, 2))
+        
+        for x in np.arange(2,23,2):
+            plt.axhline(x, zorder=z[0], color='0.9')
+        for x in quartiles:
+            plt.axvline(x, zorder=z[-1], color='r', ls='--')
+        plt.axvline(quartiles[1], ls='--', c='r', label='Quartiles')
         plt.legend()
-        plt.show()
-
-    option = input('Which graph do you want to see?\n(1) Age Distribution\n(2) Age by Party\n')
-    if option == '1':
-        print(max(age_count))
-        age_distribution()
-
-    if option == '2':
-        #age_pie()
-        pass
+        
+        plt.title('Age Representation in the 117th Congress', fontsize='xx-large')
+        plt.xlabel('Age of Members', fontsize='x-large')
+        plt.ylabel('Total Members', fontsize='x-large')
+        #plt.show()
+        print(other)
+        
+    age_distribution()
